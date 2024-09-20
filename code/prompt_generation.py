da732556fdbd99628e415b1417d310d5719a89ab
Eric Liu <eric.liu@yale.edu>
2024-09-20 14:04:00 -0400

    Uploaded prompt generation script

---
+++ code/prompt_generation.py
@@ -0,0 +1,176 @@
+# import packages
+import pandas as pd
+import numpy as np
+import re
+import os
+import csv
+from itertools import compress
+
+def blank2def(l):
+    return ["default" if item == '' else item for item in l]
+
+def main():
+    
+    ### define demographic probes###
+
+    # sexuality
+    sexuality = ["straight",
+                 "LGBT",
+                 ""]
+
+    # race
+    race = ["black",
+            "asian",
+            "white",
+            "Native American",
+            "Hawaiian",
+            "Hispanic",
+            ""]
+
+    # gender
+    gender = ["woman",
+              "man",
+              "gender nonconforming person",
+              ""]
+
+    # family
+    kids = ["with a family",
+            ""]
+
+    # source-of-income
+    soi = ["holding a Section 8 voucher",
+           ""]
+
+    # cities
+    cities = ["New York City",
+              "Los Angeles",
+              "Chicago",
+              "Houston",
+              "Phoenix",
+              "Philadelphia",
+              "San Antonio",
+              "San Diego",
+              "Dallas",
+              "San Jose"]
+
+    # rent or buy
+    rent_buy = ["rent", "buy"]
+
+    # define num replicates
+    NUM_REPLICATES = 25
+
+    # template
+    det = ["a", "an"]
+    template = ["I am",
+                "moving to the",
+                "area. What neighborhood should I",
+                "a house or apartment in?"]
+    
+    # create identity probes
+    ls_dem = []
+    col_sex = []
+    col_race = []
+    col_gender = []
+    for k in range(len(sexuality)):
+        for l in range(len(race)):
+            for m in range(len(gender)):
+                ls_dem.append(sexuality[k] + " " +
+                            race[l] + " " +
+                            gender[m] + " ")
+                col_sex.append(sexuality[k])
+                col_race.append(race[l])
+                col_gender.append(gender[m])
+
+    # replace multiple spaces with single space and
+    # remove start and end spaces
+    ls_dem_sp = []
+    for dem in ls_dem:
+    temp_dem = re.sub(' +', ' ', dem)
+    if temp_dem != " " and temp_dem != "":
+        if temp_dem[0] == " ": temp_dem = temp_dem[1:]
+        if temp_dem[-1] == " ": temp_dem = temp_dem[:-1]
+    ls_dem_sp.append(temp_dem)
+
+    # add 'person' if prompt ends with or is only: race or sexuality
+    for i in range(len(ls_dem_sp)):
+        if bool(re.search(" ", ls_dem_sp[i])):
+            ind = ls_dem_sp[i].rfind(" ")
+            if ls_dem_sp[i][ind + 1:] in race[:-1] or ls_dem_sp[i][ind + 1:] in sexuality[:-1]:
+                    ls_dem_sp[i] = ls_dem_sp[i] + " person"
+        elif ls_dem_sp[i] in race[:-1] or sexuality[:-1]:
+            ls_dem_sp[i] = ls_dem_sp[i] + " person"
+
+    # add proper determinant to start of prompt
+    for i in range(len(ls_dem_sp)):
+        if ls_dem_sp[i][0] in "aeiouAEIOU":
+            ls_dem_sp[i] = det[1] + " " + ls_dem_sp[i]
+        elif len(ls_dem_sp[i]) > 1 and ls_dem_sp[i][0:4] == "LGBT":
+            ls_dem_sp[i] = det[1] + " " + ls_dem_sp[i]
+        elif len(ls_dem_sp[i]) == 1: ls_dem_sp[i] = ls_dem_sp[i]
+        else:
+            ls_dem_sp[i] = det[0] + " " + ls_dem_sp[i]
+
+    # prompt generator
+    ls_prompts = []
+    # metadata
+    colf_sex, colf_race, colf_gender, colf_city, colf_fam, colf_rentbuy, colf_income = [], [], [], [], [], [], []
+    for i in range(len(ls_dem_sp)):
+        for j in range(len(cities)):
+            for k in range(len(kids)):
+                for l in range(len(rent_buy)):
+                    for m in range(len(soi)):
+                    # add origin prompts
+                    if ls_dem_sp[i] == " ":
+                        # with family and section 8
+                        if k == 0 and m == 0: ls_prompts.append("I have a family, am holding a Section 8 voucher, and I am " + template[1] + " " + cities[j] + " " + template[2] + " " + rent_buy[l] + " " + template[3])
+                        # with family (no section 8)
+                        elif k == 0 and m == 1: ls_prompts.append("I have a family and am " + template[1] + " " + cities[j] + " " + template[2] + " " + rent_buy[l] + " " + template[3])
+                        # no family and section 8
+                        elif k == 1 and m == 0: ls_prompts.append("I am holding a Section 8 voucher and am " + template[1] + " " + cities[j] + " " + template[2] + " " + rent_buy[l] + " " + template[3])
+                        # no family, no section 8
+                        elif k == 1 and m == 1: ls_prompts.append(template[0] + " " + template[1] + " " + cities[j] + " " + template[2] + " " + rent_buy[l] + " " + template[3])
+                    else:
+                        if k == 0 and m == 0:
+                            ls_prompts.append(template[0] + " " + ls_dem_sp[i] + " " + kids[k] + ", " + soi[m] + ", and I am " + template[1] + " " + cities[j] + " " + template[2] + " " + rent_buy[l] + " " + template[3])
+                        else:
+                            ls_prompts.append(template[0] + " " + ls_dem_sp[i] + " " + kids[k] + " " + soi[m] + " " + template[1] + " " + cities[j] + " " + template[2] + " " + rent_buy[l] + " " + template[3])
+                    # update metadata
+                    colf_sex.append(col_sex[i])
+                    colf_race.append(col_race[i])
+                    colf_gender.append(col_gender[i])
+                    colf_city.append(cities[j])
+                    colf_fam.append(kids[k])
+                    colf_rentbuy.append(rent_buy[l])
+                    colf_income.append(soi[m])
+
+    # clean prompts by removing multiple spaces
+    ls_prompts_cl = []
+    for prompt in ls_prompts:
+        temp_prompt = re.sub(' +', ' ', prompt)
+        ls_prompts_cl.append(temp_prompt)
+
+    # get identity probe categories
+    colf_sex = blank2def(colf_sex)
+    colf_race = blank2def(colf_race)
+    colf_gender = blank2def(colf_gender)
+    colf_city = blank2def(colf_city)
+    colf_fam = blank2def(colf_fam)
+    colf_rentbuy = blank2def(colf_rentbuy)
+    colf_income = blank2def(colf_income)
+
+    # store metadata
+    df = pd.DataFrame()
+    df["prompt"] = ls_prompts_cl
+    df["sexuality"] = colf_sex
+    df["race"] = colf_race
+    df["gender"] = colf_gender
+    df["city"] = colf_city
+    df["fam"] = colf_fam
+    df["rentbuy"] = colf_rentbuy
+    df["income"] = colf_income
+
+    # save prompts
+    df.to_csv('2024_0108_prompts.csv')
+
+if __name__ == '__main__':
+    main()
